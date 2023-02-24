d39355dc28e8e6cf02dd4db34fa61e6e00c6a78d
devtest00 <126182516+devtest00@users.noreply.github.com>
2023-02-24 10:22:25 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+//---- step : 1.1
+const express = require("express")
+const app = express()
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
+const cors = require('cors')
+//---- step : 3
+const multer = require("multer")
+const path = require("path")
+
+//---- step : 2.1
+const authRoute = require("./routes/auth")
+const authUser = require("./routes/user")
+const authPost = require("./routes/posts")
+const authCat = require("./routes/categories")
+const products = require('./routes/products');
+const orders = require('./routes/orders');
+
+
+//---- step : 1
+dotenv.config()
+app.use(cors())
+//---- step : 2.2
+app.use(express.json())
+//---- step : 2.3 last ma file crate garne time
+app.use("/images", express.static(path.join(__dirname, "/images")))
+
+//---- step : 1.3
+const db =  process.env.MONGODB_URI
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    /*  useCreateIndex: true,
+    useFindAndModify: true,*/
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err))
+
+//---- step : 3
+const storage = multer.diskStorage({
+  destination: (req, file, callb) => {
+    callb(null, "images")
+  },
+  filename: (req, file, callb) => {
+    //callb(null, "file.png")
+    callb(null, req.body.name)
+  },
+})
+const upload = multer({ storage: storage })
+app.post("/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded")
+})
+
+//---- step : 2
+app.use("/auth", authRoute)
+app.use("/users", authUser)
+app.use("/posts", authPost)
+app.use("/category", authCat)
+app.use('/products', products);
+app.use('/orders', orders);
+
+//---- step : 1.2
+const port = process.env.PORT
+app.listen(port, () => {
+  console.log("backend running..."+port)
+})
